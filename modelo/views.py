6d570a32f9244c5c4fe4832a60931e213d29fa44
RoldanHdz <roldanhz.27@outlook.com>
2024-11-28 03:46:05 -0600

    actufinal

--- modelo/views.py
+++ modelo/views.py
@@ -1,4 +1,5 @@
 import os
+from django.shortcuts import render
 import joblib
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
@@ -11,8 +12,35 @@ MODEL_PATH = os.path.join(os.path.dirname(__file__), 'model', 'xgboost_model_rol
 # Cargar el modelo entrenado al iniciar el servidor
 model = joblib.load(MODEL_PATH)
 
+# Diccionario de causas atribuidas al conductor
+causa_atrib_conductor = {
+    16: 'VELOCIDAD INMODERADA',
+    10: 'NO RESPETAR CEDA EL PASO',
+    14: 'OTRO',
+    5: 'CAMBIAR CARRIL SIN PRECAUCIÓN',
+    9: 'NO GUARDAR DISTANCIA',
+    12: 'NO RESPETAR SEMÁFORO',
+    4: 'CAMBIAR CARRIL SIN PRECAUCION',
+    17: 'VUELTA EN "U"',
+    15: 'REBASAR INDEBIDAMENTE',
+    13: 'NO RESPETAR SEÑAL DE ALTO',
+    11: 'NO RESPETAR SEMAFORO',
+    2: 'ALCOHOL / DROGAS',
+    6: 'CIRCULAR EN SENTIDO CONTRARIO',
+    3: 'ALCOHOL/DROGAS',
+    0: 'ABRIR PUERTA SIN PRECAUCION',
+    8: 'EXCESO DE DIMENSIONES',
+    1: 'ABRIR PUERTA SIN PRECAUCIÓN',
+    7: 'nan'
+}
+
+@csrf_exempt
+def prediction_form(request):
+    return render(request, 'predict_form.html') 
+
 @csrf_exempt
 def predict(request):
+
     if request.method == 'POST':
         try:
             # Obtener los datos JSON enviados en el cuerpo de la solicitud
@@ -29,8 +57,11 @@ def predict(request):
                 # Convertir la predicción en una lista antes de enviarla en la respuesta
                 prediction_list = prediction.tolist()
 
-                # Retornar la predicción como respuesta JSON
-                return JsonResponse({'prediction': prediction_list})
+                # Obtener el texto correspondiente a la predicción
+                causa = causa_atrib_conductor.get(prediction_list[0], "Desconocido")
+
+                # Retornar la predicción y su causa correspondiente como respuesta JSON
+                return JsonResponse({'prediction': prediction_list, 'causa': causa})
 
             else:
                 return JsonResponse({'error': 'No features provided'}, status=400)
--- prueba1/urls.py
+++ prueba1/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path
-from modelo.views import predict, test_model
+from modelo.views import predict, test_model, prediction_form
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('predict/', predict, name='predict'),  # Ruta para predicciones
     path('test/', test_model, name='test'),  # Ruta para probar el modelo
     path('test_model/', test_model, name='test_model'),
+    path('prediction/', prediction_form, name='prediction_form')
+
 ]
